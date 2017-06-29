03a9dcf2a64fa0242d2c431f238b9a00b6725038
lingjiekong91 <lingjie.kong@bsci.com>
2017-06-29 13:55:55 -0500

    this is the second test for the changing
    do not merge

--- ExtrusionDatabaseUIv3.08.R
+++ ExtrusionDatabaseUIv3.08.R
@@ -12,6 +12,7 @@ library(bootstrap)
 library(jpeg)
 library(ggplot2)
 library(DT)
+library(ggplot2)  # second test, delete later
 
 #Creating variables across all sessions
 path <- "C:/Users/correaf/Downloads/PPS_document_contents/ListOfSheets/GTE"
